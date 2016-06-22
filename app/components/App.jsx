0daf8b98920260a587b373593ae067f591811e6e
laiso <laiso@lai.so>
2016-06-22 13:35:51 +0900

    Meterial UI

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
+import { AppBar, Checkbox, Paper } from 'material-ui';
+import { List, ListItem } from 'material-ui/List';
+
 require('./App.css');
 
-export default () => <h1>Hello World</h1>;
-
-/*
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <h1>Hello World</h1>
-    );
-  }
-}
-*/
+export default () => (
+    <div>
+        <AppBar title="Tasks"/>
+        <Paper>
+            <List>
+                <ListItem primaryText="Item 1" leftCheckbox={<Checkbox />}/>
+                <ListItem primaryText="Item 2" leftCheckbox={<Checkbox />}/>
+                <ListItem primaryText="Item 3" leftCheckbox={<Checkbox />}/>
+            </List>
+        </Paper>
+    </div>
+);
--- app/main.jsx
+++ app/main.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+
 import App from './components/App.jsx';
 
 ReactDOM.render(
-  <App />,
+  <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+      <App />
+  </MuiThemeProvider>,
   document.body.appendChild(document.createElement('div'))
 );
